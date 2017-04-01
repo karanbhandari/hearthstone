8732f027a0ead23339728ec25e5c8cb750e6dafd
karanbhandari <karan7752@gmail.com>
2017-04-01 14:25:37 -0400

    abilities minor changes pushed

--- Ability.cc
+++ Ability.cc
@@ -22,6 +22,7 @@ void TriggeredAbility::performAbility() {
 void TriggeredAbility::startOfTurn() {
 	// triggeres all the startof turn abilities
 	// 1: Restores the action to minion to 1, and it
+	
 }
 
 void TriggeredAbility::endOfTurn() {
@@ -29,11 +30,17 @@ void TriggeredAbility::endOfTurn() {
 }
 
 void TriggeredAbility::minionEnter() {
-
+	if(name == "Whenever an opponent’s minion enters play, deal 1 damage to it."){
+		// reduce opponent'minion's defence by one on enter. (this is fire elemental)
+	}
 }
 
 void TriggeredAbility::minionLeave() {
-
+	if(name == "Gain +1/+1 whenever a minion leaves play."){
+		// reduce this increase attack and defence by 1(this is Bone Golem)
+	} else {
+		// 
+	}
 }
 
 ActivatedAbility::ActivatedAbility(const string& nameOfAbility) : Ability{nameOfAbility}{
@@ -41,5 +48,5 @@ ActivatedAbility::ActivatedAbility(const string& nameOfAbility) : Ability{nameOf
 }
 
 void ActivatedAbility::performAbility() {
-
+	
 }
