4a9fb04b05155b45cc13734eb150cbd875f645f4
karanbhandari <k3bhanda@student.cs.uwaterloo.ca>
2017-04-03 22:41:13 -0400

    main console fix

--- main.cc
+++ main.cc
@@ -24,7 +24,7 @@ void mainLoop(istream *in, bool testing, Board* board, Player *activePlayer) {
 		istringstream iss{s};
 		iss >> s;
 		if (s == "end") { // end players turn
-			cout << s << endl;
+			cout << "Next Player's turn." << endl;
 			activePlayer->performEndTrigger(activePlayer, inactivePlayer);
 			inactivePlayer = activePlayer;
 			if (activePlayer == board->p1) {
@@ -34,12 +34,12 @@ void mainLoop(istream *in, bool testing, Board* board, Player *activePlayer) {
 			}
 			activePlayer->performStartTrigger(activePlayer, inactivePlayer);
 		} else if (s == "quit") { // end the game with no winner
-			cout << "quit" << endl;
+			cout << "Quitting" << endl;
 			delete board;
 			break;
 		} else if (s == "draw") { // draws a card (only in testing mode) 
 			if (testing) {
-				cout << "draw" << endl;
+				// cout << "draw" << endl;
 				activePlayer->draw();
 			} else { 
 				cout << "testing should be enabled for this command" << endl;
@@ -47,7 +47,7 @@ void mainLoop(istream *in, bool testing, Board* board, Player *activePlayer) {
 		} else if (s == "discard") { // remove ith card from the players hand (only in Tmode)
 			iss >> i;
 			if (testing) {
-				cout << "discard " << i << endl;
+				// cout << "discard " << i << endl;
 				activePlayer->discard(i);
 			} else { 
 				cout << "testing should enabled for this command" << endl;
@@ -55,11 +55,11 @@ void mainLoop(istream *in, bool testing, Board* board, Player *activePlayer) {
 		} else if (s ==  "attack") { // attack the opponent
 			iss >> i; // active players minion
 			if (iss >> j) { // attack the minion j of opposition
-				cout << "attack on " << j << " by " << i << endl;
+				// cout << "attack on " << j << " by " << i << endl;
 				activePlayer->attack(i, inactivePlayer, j, activePlayer);
 			} else { // attack on the opposite player
 				iss.clear();
-				cout << "attack on player by " << i << endl;
+				// cout << "attack on player by " << i << endl;
 				activePlayer->attack(i, inactivePlayer);
 			}
 		} else if (s == "play") { // to play a card i
@@ -70,7 +70,7 @@ void mainLoop(istream *in, bool testing, Board* board, Player *activePlayer) {
 					iss.clear();
 					l = 114;
 				}
-				cout << "play " << i << "th card on player " << j << "s " << l << endl;
+				// cout << "play " << i << "th card on player " << j << "s " << l << endl;
 				if(j == 1) {
 					activePlayer->play(i, board->p1, l, activePlayer, inactivePlayer);
 				} else {
@@ -78,39 +78,39 @@ void mainLoop(istream *in, bool testing, Board* board, Player *activePlayer) {
 				}
 			} else {
 				iss.clear();
-				cout << "play " << i << "the card" << endl;
+				// cout << "play " << i << "the card" << endl;
 				activePlayer->play(i, activePlayer, inactivePlayer);
 			}
 		} else if (s == "use") { // use activated ability 
 			iss >> i;
 			if (iss >> j) {
 				iss >> k;
-				cout << "use " << i << "th minion on player " << j << "s " << k << endl;
+				// cout << "use " << i << "th minion on player " << j << "s " << k << endl;
 				if(j == 1) {
 					activePlayer->use(i, board->p1, k, activePlayer, inactivePlayer);
 				} else {
 					activePlayer->use(i, board->p2, k, activePlayer, inactivePlayer);
 				}
 			} else {
-				cout << "use " << i << "the minion" << endl;
+				// cout << "use " << i << "the minion" << endl;
 				iss.clear();
 				activePlayer->use(i, activePlayer, inactivePlayer);
 			}
 		} else if (s == "inspect") { // inspect the ith minion owned by the player
 			iss >> i;
-			cout << "inspect the " << i << "th minion" << endl;
+			// cout << "inspect the " << i << "th minion" << endl;
 		} else if (s == "hand") { //display the activePlayers Hand
-			cout << "hand of active" << endl;
+			// cout << "hand of active" << endl;
 			activePlayer->showHand();
 			printHand(activePlayer);
 		} else if (s == "board") { //display the board
-			cout << "wrong command" << endl;
+			// cout << "wrong command" << endl;
 			printBoard(activePlayer, inactivePlayer);
 			//activePlayer->showDeck();
 		} else {
 			cout << "INVALID COMMAND" << endl;
 		}
-		if(dbg) cout << inactivePlayer->getName() << "s life: " <<inactivePlayer->life <<endl;
+		// if(dbg) cout << inactivePlayer->getName() << "s life: " <<inactivePlayer->life <<endl;
 		activePlayer->moveToGraveyard();
 		if (!inactivePlayer->isAlive()) {
 			cout << inactivePlayer->getName() << " loses" <<endl;
